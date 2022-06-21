44ba8d7dd8bfcb64eba12b32884b8d41f842e6da
jesse <itsjessef@gmail.com>
2022-06-21 15:29:05 -0700

    starter files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/App.scss";
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>Hello, this is the starter file.</h2>
+    </div>
+  );
+}
+
+export default App;
