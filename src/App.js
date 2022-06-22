9f4e256d67550a6f4ffe88921af8d5cce722f264
jesse <itsjessef@gmail.com>
2022-06-21 23:41:05 -0700

    add UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles/App.scss";
+import Header from "./components/header.js";
+import Banner from "./components/banner.js";
+import Cases from "./components/cases.js";
 
 function App() {
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }, []);
+
   return (
     <div className='App'>
-      <h2>Hello, this is the starter file.</h2>
+      <Header />
+      <Banner />
+      <Cases />
     </div>
   );
 }
---
+++ src/components/cases.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { ReactComponent as CasesNext } from "../assets/arrow-right.svg"
+import { ReactComponent as CasesPrev } from "../assets/arrow-left.svg"
+
+const caseStudies = [
+  {
+    id: 1,
+    subtitle: "Indoors",
+    title: "Plants in a fun but elegant pot, perfect for your home",
+    img: "home",
+    desc: "Small plant in decorative pot"
+  },
+  {
+    id: 2,
+    subtitle: "Hybrid",
+    title: "Plants suited for any space, outdoors or indoors",
+    img: "space",
+    desc: "Variety of size plants in a porch"
+  },
+  {
+    id: 3,
+    subtitle: "Outdoors",
+    title: "Plants to keep your garden space full of life",
+    img: "outdoor",
+    desc: "Plants living in an garden"
+  }
+]
+
+const Cases = () => {
+  return (
+    <section className="cases">
+      <div className="container-fluid">
+        <div className="cases-navigation">
+          <div className="cases-arrow prev disabled">
+            <CasesPrev />
+          </div>
+          <div className="cases-arrow next">
+            <CasesNext />
+          </div>
+        </div>
+        <div className="row">
+          {caseStudies.map(caseItem => (
+            <div className="case" key={caseItem.id}>
+              <div className="case-details">
+                <span>{caseItem.subtitle}</span>
+                <h2>{caseItem.title}</h2>
+              </div>
+              <div className="case-image">
+                <img
+                  src={require(`../assets/${caseItem.img}.jpg`)}
+                  alt={caseItem.desc}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Cases;
